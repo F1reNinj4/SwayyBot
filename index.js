39165c0dbc9b8afecfb997bfc36c193e9540dd35
F1reNinj4 <59781418+F1reNinj4@users.noreply.github.com>
2020-01-12 18:50:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const ms = require('ms');
+const fs = require('fs');
+
+module.exports = { version : '1.0.2' };
+
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    bot.commands.set(command.name, command);
+}
+
+const token = 'NjY1NjI4MjI1OTE3MTU3Mzg4.Xhpyvg.vfF5gg-ExjGbZ7KQFJ30_Cly4Oo';
+
+const PREFIX = '?'
+
+bot.on('ready', () => {
+    console.log('This bot is online!')
+})
+
+bot.on('message', message => {
+
+    let args = message.content.substring(PREFIX.length).split(' ');
+
+    switch (args[0]) {
+        case 'ping':
+            bot.commands.get('ping').execute(message, args);
+            break;
+        case 'info':
+            bot.commands.get('info').execute(message, args);
+            break;
+        case 'help':
+            bot.commands.get('help').execute(message, args);
+            break;
+        case 'admin':
+            bot.commands.get('admin').execute(message, args);
+            break;
+    }
+})
+
+bot.login(token);
