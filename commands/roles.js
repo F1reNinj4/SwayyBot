e0ae1d0e71c3ba3e4ad33378803f5a6905ff26b4
F1reNinj4 <59781418+F1reNinj4@users.noreply.github.com>
2020-01-30 17:02:41 +0000

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -12,10 +12,10 @@ module.exports = {
                 if (embedMsg) {
                     message.react(':Minecraft:667493014360555530')
                     .then(reaction => reaction.message.react(':RocketLeague:667493014137995314'))
-                    .then(reaction => reaction.message.react('<:osu:672483428276502539>'))
-                    .then(reaction => reaction.message.react('<:HunterCallOfTheWild:672483427555213332>'))
-                    .then(reaction => reaction.message.react('<:CallOfDuty:672483428809048064>'))
-                    .then(reaction => reaction.message.react('<:Rainbow6:672483425827160084>'))
+                    .then(reaction => reaction.message.react(':osu:672483428276502539'))
+                    .then(reaction => reaction.message.react(':HunterCallOfTheWild:672483427555213332'))
+                    .then(reaction => reaction.message.react(':CallOfDuty:672483428809048064'))
+                    .then(reaction => reaction.message.react(':Rainbow6:672483425827160084'))
                     .then(reaction => reaction.message.delete(20000))
                 }
             }
@@ -30,7 +30,7 @@ module.exports = {
         '<:osu:672483428276502539> - osu!\n' +
         '<:HunterCallOfTheWild:672483427555213332> - Hunter: Call of the Wild\n' +
         '<:CallOfDuty:672483428809048064> - Call of Duty\n' +
-        '<:Rainbow6:672483425827160084> Rainbow 6 Siege\n');
+        '<:Rainbow6:672483425827160084> - Rainbow 6 Siege\n');
         embed.setFooter('SwayyBot v' + version);
 
         message.channel.send(embed);
